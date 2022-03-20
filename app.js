23011601a24771103e1e47bde58aaa533d5a6499
osamaahmed17 <osamaahmedtahir17@gmail.com>
2022-03-20 05:33:57 +0500

    Server and Client Added

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require("express");
+const path = require('path');
+const yahooStockAPI  = require('yahoo-stock-api');
+
+
+
+async function stockDetail()  {
+	return await  yahooStockAPI.getSymbol('AAPL');
+}
+
+
+
+var app = express();
+// Serve static files from the React app
+//app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+app.get('/express_backend', async (req, res) => {
+    async function stockHistory()  {
+        const startDate = new Date('08/21/2020');
+        const endDate = new Date('08/26/2020');
+        return await yahooStockAPI.getHistoricalPrices(startDate, endDate, 'AAPL', '1d');
+    }
+   var stockHistory = await stockHistory();
+   res.send({ express: stockHistory });
+
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+
+
+//Server Setup
+if (process.env.PORT) {
+    server = app.listen(process.env.PORT || 5000, process.env.IP, function() {
+        console.log("Server is running");
+    });
+} else {
+    server = app.listen(5000, function() {
+        console.log("Server is running");
+    });
+}
